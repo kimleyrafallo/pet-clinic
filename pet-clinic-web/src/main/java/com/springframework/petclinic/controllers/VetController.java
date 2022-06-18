efa9b59b6df114a9aff62c6903efcc1c216871f7
kimleyrafallo <earlkimleyrafallo@gmail.com>
2022-06-18 23:11:05 +0800

    Added diplaying of list of vets to vets index page

--- pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package com.springframework.petclinic.controllers;
 
+import com.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
-        import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listOfVets(){
+    public String listOfVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
