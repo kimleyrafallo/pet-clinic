a861d2aec3946b070b7fc88839b3ef3deca82259
kimleyrafallo <earlkimleyrafallo@gmail.com>
2022-06-30 09:29:02 +0800

    Convert Owners to JPA Entities

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Vet.java
@@ -1,10 +1,14 @@
 package com.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
 
+    @ManyToMany(mappedBy = "specialty")
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Visit.java
@@ -1,11 +1,19 @@
 package com.springframework.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visit")
 public class Visit extends BaseEntity {
 
     private LocalDate date;
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
