5083d4128bab1615772df10a1f489576c26d0831
kimleyrafallo <earlkimleyrafallo@gmail.com>
2022-06-30 11:48:07 +0800

    Create Spring data JPA Service for Pet Service

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/springdataJPA/PetSDJPA.java
@@ -0,0 +1,45 @@
+package com.springframework.petclinic.services.map.springdataJPA;
+
+import com.springframework.petclinic.model.Pet;
+import com.springframework.petclinic.repositories.PetRepository;
+import com.springframework.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJPA implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJPA(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return (Set<Pet>) petRepository.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
