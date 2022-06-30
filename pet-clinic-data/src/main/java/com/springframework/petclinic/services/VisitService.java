94d90f0ad6fc44b1467ca6faf74d4554ba93a4b8
kimleyrafallo <earlkimleyrafallo@gmail.com>
2022-06-30 12:20:24 +0800

    Create Visit Map Service

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.springframework.petclinic.services;
+
+import com.springframework.petclinic.model.Vet;
+import com.springframework.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
